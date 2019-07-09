8b9ef4b763e967f0a3de44bb9b374190147cd8ab
4garfield <songbintaochina@gmail.com>
2019-07-10 00:07:37 +0800

    pwa update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+import { PlatformService } from './services/platform.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  promptEvent;
+  updateAvailable = false;
+
+  constructor(private platformService: PlatformService, private swUpdate: SwUpdate) {
+    if (this.platformService.isBrowser()) {
+      window.addEventListener('beforeinstallprompt', event => {
+        this.promptEvent = event;
+      });
+    }
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        this.updateAvailable = true;
+      });
+    }
+  }
+
+  installApp(): void {
+    this.promptEvent.prompt();
+  }
+
+  doUpdate() {
+    if (this.platformService.isBrowser()) {
+      window.location.reload();
+    }
+  }
 
 }
