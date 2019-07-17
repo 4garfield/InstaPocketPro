e319736d90787802f58394048a9b5d8bd8c8b672
4garfield <songbintaochina@gmail.com>
2019-07-17 21:57:37 +0800

    update firebase

---
+++ functions/api.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+const router = express.Router().get('/', function (req, res) {
+  res.json({
+    message: 'hello world!'
+  });
+});
+
+app.use('/api', router);
+
+// catch 404
+app.use(function (req, res, next) {
+  res.status(404);
+  res.json({
+    msg: 'Request Resource Not Found'
+  });
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    msg: err.message
+  });
+});
+
+exports.api = app;
--- functions/firebase.js
+++ functions/firebase.js
@@ -3,4 +3,8 @@ const functions = require('firebase-functions');
 process.env.DIST = './';
 const app = require('./server').app;
 
+const api = require('./api').api;
+
 exports.ssr = functions.https.onRequest(app);
+
+exports.api = functions.https.onRequest(api);
